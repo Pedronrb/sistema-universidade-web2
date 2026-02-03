72e8494e898a7cc4dd4d358c4afb3314b4a408b7
ailtonjaj <ailton.junior@ccc.ufcg.edu.br>
2026-02-03 19:36:35 -0300

    implementando rateliming na rota de login

--- src/middlewares/loginLimiter.js
+++ src/middlewares/loginLimiter.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 export const loginLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 5,
-  message: "Muitas tentativas de login. Tente novamente me 1 minuto."
+  message: "Muitas tentativas de login. Tente novamente me 1 minuto.",
   standardHeaders: true,
   legacyHeaders: false,
 });
